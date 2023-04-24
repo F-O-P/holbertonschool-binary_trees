1ac07c18474abe5e28eb31e0169b75c809531f2c
F-O-P <tayler.coon@gmail.com>
2023-04-24 05:40:46 -0700

    task 8 functional & betty compliant

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using
+ * post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function for each node
- * 
+ *
  * Return: void
  */
 
