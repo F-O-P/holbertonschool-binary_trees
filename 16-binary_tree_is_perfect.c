64e5b843c1c07b05981c462f8f678c395f39399b
F-O-P <tayler.coon@gmail.com>
2023-04-24 08:15:12 -0700

    and again, 2, 3 ,4

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -39,20 +39,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left) + 1;
-	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right) + 1;
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return (left_height);
+		return (left_height + 1);
 	else
-		return (right_height);
+		return (right_height + 1);
 }
 
